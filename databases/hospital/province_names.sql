baa5bb9503e85ef242293fc57911d1920f13bb5b
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-08-09 23:48:17 -0500

    Add the scripts to populate the data to each table

---
+++ databases/hospital/province_names.sql
@@ -0,0 +1,15 @@
+INSERT INTO province_names (province_id, province_name)
+VALUES
+  ('AB', 'Alberta'),
+  ('BC', 'British Columbia'),
+  ('MB', 'Manitoba'),
+  ('NB', 'New Brunswick'),
+  ('NL', 'Newfoundland and Labrador'),
+  ('NT', 'Northwest Territories'),
+  ('NS', 'Nova Scotia'),
+  ('NU', 'Nunavut'),
+  ('ON', 'Ontario'),
+  ('PE', 'Prince Edward Island'),
+  ('QC', 'Quebec'),
+  ('SK', 'Saskatchewan'),
+  ('YT', 'Yukon');
