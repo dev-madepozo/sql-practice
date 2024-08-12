d28d2ba5beba0627b550e6333ed83d0122e4c5a7
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-08-11 21:29:11 -0500

    Create tables customers & regions

--- databases/northwind/db.sql
+++ databases/northwind/db.sql
@@ -12,3 +12,28 @@ CREATE TABLE categories(
   description VARCHAR(100) NOT NULL,
   PRIMARY KEY (category_id)
 );
+
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE customers(
+  customer_id VARCHAR(5) NOT NULL,
+  customer_name VARCHAR(50) NOT NULL,
+  contact_name VARCHAR(50) NOT NULL,
+  contact_title VARCHAR(50) NOT NULL,
+  address VARCHAR(50) NOT NULL,
+  city VARCHAR(25) NOT NULL,
+  region VARCHAR(25) NOT NULL,
+  postal_code VARCHAR(10) NOT NULL,
+  country VARCHAR(25) NOT NULL,
+  phone VARCHAR(25) NOT NULL,
+  fax VARCHAR(25) NOT NULL,
+  PRIMARY KEY (customer_id)
+);
+
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE regions(
+  region_id INT NOT NULL AUTO_INCREMENT,
+  region_description VARCHAR(30) NOT NULL,
+  PRIMARY KEY (region_id)
+);
