b2a4af409fb4351726e423a2223f063af4c363f3
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-08-11 21:01:42 -0500

    Solve northwind easy > questions 01 & 02

---
+++ questions/northwind/easy/01.sql
@@ -0,0 +1,9 @@
+/*
+  Ease > Question 01
+
+  Show the category_name and description from the categories table sorted by category_name.
+*/
+
+SELECT category_name, description
+FROM categories
+ORDER BY category_name;
---
+++ questions/northwind/easy/02.sql
@@ -0,0 +1,10 @@
+/*
+  Ease > Question 02
+
+  Show the employee_id, order_id, customer_id, required_date, shipped_date
+  from all orders shipped later than the required date.
+*/
+
+SELECT category_name, description
+FROM categories
+ORDER BY category_name;
